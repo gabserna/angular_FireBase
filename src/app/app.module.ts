5da245242d9df6404b09a7d87a2e4de81bfd5ced
Gabriel Serna <gabserna@hotmail.com>
2023-09-08 10:12:27 -0700

    x

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -7,8 +8,12 @@ import { MatToolbarModule } from "@angular/material/toolbar";
 import { environment} from "../../environment";
 import { AngularFireModule } from "@angular/fire/compat";
 import { AngularFirestoreModule } from "@angular/fire/compat/firestore";
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { CompanyEditComponent } from './company/company-edit/company-edit.component';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { CompanyEditComponent } from './company/company-edit/company-edit.component'
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,11 +23,15 @@ import { CompanyEditComponent } from './company/company-edit/company-edit.compon
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,    
+    BrowserAnimationsModule,
     MatToolbarModule,
-    AngularFireModule.initializeApp(environment.firebase), // <-- add this
+    AngularFireModule.initializeApp(environment.firebase), 
     AngularFirestoreModule,
     FlexLayoutModule,
+    MatCardModule, 
+    MatButtonModule,
+    MatFormFieldModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/company/company-edit/company-edit.component.ts
+++ src/app/company/company-edit/company-edit.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {AngularFirestore} from "@angular/fire/compat/firestore";
 import { Observable } from 'rxjs';
 import { Company } from '../../models/company';
+import { CompanyService } from '../company.service';
 
 @Component({
   selector: 'app-company-edit',
@@ -10,12 +11,16 @@ import { Company } from '../../models/company';
 })
 export class CompanyEditComponent implements OnInit {
 
-  company$: Observable<any>;
+  company$: Observable<Company>;
 
   constructor(private db: AngularFirestore) {
     this.company$ = this.db.doc<Company>('companies/6niW63kpjzCXdEt28JXE').valueChanges();
   }
-
+  
   ngOnInit() {}
+  
+  saveCompany(company) {
+    this.companyService.saveCompany(company);
+  }
 
 }
